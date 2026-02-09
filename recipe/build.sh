f31d42568364427b93705e1ff48272f06f6ea2c2
Ben Moss <bmoss40@bloomberg.net>
2026-02-09 12:45:20 -0500

    use cross-python

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,8 +6,8 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == "1" ]]; then
     export PYO3_CROSS=1
     export PYO3_CROSS_LIB_DIR="${PREFIX}/lib"
     export PYO3_CROSS_PYTHON_VERSION=${PY_VER}
-    "${BUILD_PREFIX}/bin/maturin" build --release --strip --target $CARGO_BUILD_TARGET -i "${PY_VER}" --out dist
-    ${PYTHON} -m pip install dist/*.whl --no-deps -vv
+    "${BUILD_PREFIX}/bin/maturin" build --release --strip --target $CARGO_BUILD_TARGET -i "${BUILD_PREFIX}/bin/python" --out dist
+    "${BUILD_PREFIX}/bin/python" -m pip install dist/*.whl --no-deps --no-build-isolation --prefix="${PREFIX}" --no-index -vv
 else
     $PYTHON -m pip install . -vv
 fi
