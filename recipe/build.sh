c280bf8817158727fc437cb9312ed6fe4094aa24
Ben Moss <bmoss40@bloomberg.net>
2026-02-09 12:14:25 -0500

    fix maturin for cross compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == "1" ]]; then
     export PYO3_CROSS_PYTHON_VERSION=${PY_VER}
-    "${BUILD_PREFIX}/bin/maturin" build --release --strip --interpreter "python${PY_VER}" --out dist
+    "${BUILD_PREFIX}/bin/maturin" build --release --strip --interpreter "python${PY_VER}" --target $CARGO_BUILD_TARGET --out dist
     ${PYTHON} -m pip install dist/*.whl --no-deps -vv
 else
     $PYTHON -m pip install . -vv
